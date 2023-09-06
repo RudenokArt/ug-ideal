9586cdbbec17a954deb5789bab1893f796d01be0
rudenokArt <rudenokArt@yandex.ru>
2023-09-06 14:52:54 +0300

    new search form

--- ug_ideal-includes/search-form.php
+++ ug_ideal-includes/search-form.php
@@ -1,10 +1,9 @@
 <form action="" method="get" class="row">
-  <div class="col-8">
-    <input type="text" name="search" class="form-control" placeholder="Поиск по артикулу">
-  </div>
-  <div class="col-4">
-    <button class="btn btn-lg btn-outline-info">
-      <i class="fa fa-search" aria-hidden="true"></i>
-    </button>
-  </div>        
+  <div class="input-group mb-3">
+  <input type="text" name="search" class="form-control" placeholder="Поиск по артикулу">
+  <button class="input-group-text">
+    <i class="fa fa-search" aria-hidden="true"></i>
+  </button>
+</div>  
 </form>
+
